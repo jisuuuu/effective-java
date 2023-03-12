5f3c429ecee5ca4189d9603d3ddb2899c03fd3e3
jisuuuu <gjkl55@naver.com>
2023-03-12 20:35:03 +0900

    effective java - item 33

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Iphone iphone = new Iphone();
-        iphone.process();
+        Favorites favorites = new Favorites();
 
-        GalaxyPhone galaxyPhone = new GalaxyPhone();
-        galaxyPhone.process();
+        Favorites f = new Favorites();
+
+        f.putFavorite(String.class, "Java");
+        f.putFavorite(Integer.class, 0xcafebabe);
+        f.putFavorite(Class.class, Favorites.class);
+
+        String favoriteString = f.getFavorite(String.class);
+        int favoriteInteger = f.getFavorite(Integer.class);
+        Class<?> favoriteClass = f.getFavorite(Class.class);
+
+        System.out.printf("%s %x %s%n", favoriteString, favoriteInteger, favoriteClass.getName());
+
+//        f.putFavorite((Class) Integer.class, "hi");
+//        int x = f.getFavorite(Integer.class);
+
+//        f.putFavorite(List<Integer>.class, "hi");
     }
 }
