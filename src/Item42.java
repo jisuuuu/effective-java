a4f0ff3c82943651f88ca02025cf49b96f7395f7
jisuuuu <gjkl55@naver.com>
2023-03-16 20:19:19 +0900

    effective java - item 42

--- src/Item42.java
+++ src/Item42.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Item42 {
+import java.util.*;
+
+public class Item42 {
+    public static void anonymousClass() {
+        ArrayList<String> words = new ArrayList<>(List.of("a", "b", "c"));
+
+        Collections.sort(words, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+    }
+
+    public static void lambda() {
+        ArrayList<String> words = new ArrayList<>(List.of("a", "b", "c"));
+
+        Collections.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+    }
+
+    public static void main(String[] args) {
+        anonymousClass();
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public enum Menu {
+import java.util.function.Function;
+
+public enum Menu {
+
+    COFFEE("커피", money -> money * 2),
+    CAKE("케이크", money -> money),
+    TEA("차", money -> money * 3);
+
+    private final String name;
+    private final Function<Integer, Integer> op;
+
+    Menu(String name, Function<Integer, Integer> op) {
+        this.name = name;
+        this.op = op;
+    }
+
+    public int price(int money) {
+        return op.apply(money);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
